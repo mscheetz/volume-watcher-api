6e7c4eaaef9ad73d4ae2ea8fc1cb1e06c7a74c00
Matt Scheetz <mfscheetz@gmail.com>
2020-04-13 21:35:09 -0400

    call exchange from here

--- services/volume.service.js
+++ services/volume.service.js
@@ -1,2 +1,21 @@
+const binanceSvc = require('./binance.service');
+const _sizes = [ "1h", "1d" ];
 
+const runVolumeCheck = async() => {
+    await binanceSvc.cleanMe();
+    await binanceSvc.runCheck(_sizes);
+}
 
+const customRun = async(exchange, size, percent) => {
+    let result = [];
+    if(exchange === "BINANCE") {
+        result = await binanceSvc.customRun(size, percent);
+    }
+
+    return result;
+}
+
+module.exports = {
+    runVolumeCheck,
+    customRun
+}
