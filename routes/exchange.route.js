61d693df5462a645cc06fdd03875b56190b295e0
Matt Scheetz <mfscheetz@gmail.com>
2020-04-14 11:04:39 -0400

    exchange endpoints

--- routes/exchange.route.js
+++ routes/exchange.route.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const candleRepo = require('../data/candle.repo');
 
 router.use(async(req, res, next) =>{
-    console.log('router called');
+    console.log('exchange router called');
     next();
 });
 
 router.get('/', async(req, res, next) => {
-    const message = "hello world from this api endpoint";
+    const exchanges = [ "BINANCE" ];
 
-    res.json(message);
+    res.json(exchanges);
 });
+
+router.get('/intervals', async(req, res, next) => {
+    const intervals = [ "1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M" ];
+
+    res.json(intervals);
+})
 
 module.exports = router;
