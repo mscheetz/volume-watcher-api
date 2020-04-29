f80d1921910bf4d539656eac48a5f14dbbfd146c
Matt Scheetz <mfscheetz@gmail.com>
2020-04-29 19:35:25 -0400

    days over check

--- services/binance.service.js
+++ services/binance.service.js
@@ -2,6 +2,7 @@ const Binance = require('node-binance-api');
 const binance = new Binance();
 const _ = require('lodash');
 const volumeRepo = require('../data/volume-watch.repo');
+const volumeIncrRepo = require('../data/volume-increase.repo');
 const amqRepo = require('./queue.broker');
 const coreSvc = require('./core.service');
 const config = require('../config');
@@ -10,9 +11,14 @@ const basePercent = config.BASE_PERCENT;
 let _volumePercent = config.BASE_PERCENT;
 let promisesRecvd = 0;
 let broker;
+let increments = [];
 
 const cleanMe = async() => {    
     await volumeRepo.cleanExchange(_exchange);
+}
+
+const cleanMeIncr = async() => {    
+    await volumeIncrRepo.cleanExchange(_exchange);
 }
 
 const customRun = async(size, percent, btc, usdt) => {
@@ -52,6 +58,21 @@ const runCheck = async(sizes) => {
     await Promise.all(promises);
 }
 
+const runOverageCheck = async() => {
+    console.info(`Running ${_exchange} Overage check`)
+
+    const pairs = await getTradingPairs();
+    let promises = [];
+    let sizes = [ "1d", "3d", "1w" ];
+
+    increments = [];
+    sizes.forEach(size => {
+        promises.push(findDaysOverAverage(pairs, size));
+    });
+
+    await Promise.all(promises);
+}
+
 const getTradingPairs = async(btc = true, usdt = true) => {
     try{
         const exchangeInfo = await binance.exchangeInfo();
@@ -86,22 +107,82 @@ const findIndicators = async(pairs, size, custom = false, uuid = "", callback = 
                     const addIndicator = await coreSvc.volumeVerify(pair, sticks, _volumePercent);
 
                     if(addIndicator) {
+                        const indicator = createCustomVolumeWatch(sticks, pair, size);
 
                         if(!custom) {
-                            const indicator = createCustomVolumeWatch(sticks, pair, size);
                             await volumeRepo.add(indicator);
                         } else {
-                            const indicator = createCustomVolumeWatch(sticks, pair, size);
                             await sendToQueue(uuid, indicator);
                         }
-                    } else {
                     }
                 }
-                
-            } else {
             }
             promisesRecvd++;
           }, { limit: 100 });
+    }
+}
+
+const findDaysOverAverage = async(pairs, size, custom = false, uuid = "", callback = null) => {
+    for await (const pair of pairs) {
+        console.log(`calling ${pair} for  ${size}`);
+        binance.candlesticks(pair, size, async(err, ticks, pair) => {
+            if(typeof ticks !== 'undefined' && ticks.length > 0) {
+                let sticks = getTicks(ticks, size);
+                sticks = sticks.reverse();
+                if(sticks.length > 0) {
+                    const daysOver = await coreSvc.sticksOverAverage(pair, sticks, size);
+                    const sizeIndicator = size === "1d" 
+                        ? createCustomVolumeWatch(sticks, pair, size) 
+                        : null;
+                    const thisIndicator = {
+                        pair: pair,
+                        size: size,
+                        indicator: sizeIndicator,
+                        daysOver: daysOver.overs,
+                        volume: sticks.splice(0, 50).map(s => s.volume),
+                        volumeAverages: daysOver.avgs
+                    };
+                    increments.push(thisIndicator);
+                    //console.log(`adding ${pair} for ${size}`)
+                    
+                    const match = increments.filter(i => i.pair === pair);
+                    if(match.length > 2) {
+                        //console.log(pair, match);
+                        const oneDay = match.filter(m => m.size === "1d")[0];
+                        const threeDay = match.filter(m => m.size === "3d")[0];
+                        const oneWeek = match.filter(m => m.size === "1w")[0];
+                        if(typeof oneDay !== 'undefined' && typeof threeDay !== 'undefined' && typeof oneWeek !== 'undefined') {
+                            let daysOver = oneDay.daysOver;
+                            daysOver.push(threeDay.daysOver[0]);
+                            daysOver.push(oneWeek.daysOver[0]);
+                            let volumeAvgs = oneDay.volumeAverages;
+                            volumeAvgs.push(threeDay.volumeAverages[0]);
+                            volumeAvgs.push(oneWeek.volumeAverages[0]);
+                            const max = Math.max(...daysOver);
+
+                            if(daysOver.length > 0 && max > 0) {
+                                let indicator = oneDay.indicator;
+                                indicator.daysOver = daysOver;
+                                indicator.volume1d = oneDay.volume;
+                                indicator.volume3d = threeDay.volume;
+                                indicator.volume1w = oneWeek.volume;
+                                indicator.volumeAverages = volumeAvgs;
+                                delete indicator.volume;
+                                //const indicator = createCustomVolumeWatch(sticks, pair, size, daysOver);
+                                if(!custom) {
+                                    await volumeIncrRepo.add(indicator);
+                                } else {
+                                    await sendToQueue(uuid, indicator);
+                                }
+                            }
+                            let removeMe = [ pair ];
+                            increments = increments.filter(i => !removeMe.includes(i.pair));
+                        }
+                    } 
+                }
+            }
+            promisesRecvd++;
+          }, { limit: 1000 });
     }
 }
 
@@ -109,7 +190,7 @@ const sendToQueue = async(queue, message) => {
     await broker.send(queue, message);
 }
 
-const createCustomVolumeWatch = function(sticks, pair, size) {
+const createCustomVolumeWatch = function(sticks, pair, size, daysOver = []) {
     let volumes = sticks.map(s => s.volume);
     volumes = volumes.reverse();
 
@@ -124,6 +205,10 @@ const createCustomVolumeWatch = function(sticks, pair, size) {
         closeTime: sticks[0].closeTime,
         volume: volumes
     };
+
+    if(daysOver.length > 0) {
+        obj.daysOver = daysOver;
+    }
 
     return obj;
 }
@@ -252,7 +337,9 @@ const getTick = function(tick, size) {
 
 module.exports = {
     cleanMe,
+    cleanMeIncr,
     runCheck,
+    runOverageCheck,
     customRun,
     customRunNoQueue
 }
